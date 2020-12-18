efe4a665d91f9fab32d39493e42bd9b667fa6bd1
Karel-de-Kleine <62249850+Karel-de-Kleine@users.noreply.github.com>
2020-12-18 14:06:35 +0100

    Toggle button added
    Toggle button changes collor and text

--- HMI/HMI_PI.py
+++ HMI/HMI_PI.py
@@ -5,34 +5,42 @@
 # Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
-
+import roslib
+import rospy
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Form(object):
+    def Bttn_mode(self):
+        if self.Button_Mode.isChecked():
+            self.Button_Mode.setStyleSheet("Background-color : Green")
+            self.Button_Mode.setText("Automatische Besturing")
+        else:
+            self.Button_Mode.setStyleSheet("Background-color : Red")
+            self.Button_Mode.setText("Handmatige Besturing")
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(403, 482)
         self.Button_Mode = QtWidgets.QPushButton(Form)
         self.Button_Mode.setGeometry(QtCore.QRect(0, 240, 401, 231))
-        self.Button_Mode.setStyleSheet("Red")
+        self.Button_Mode.setStyleSheet("Background-color : Red")
         self.Button_Mode.setCheckable(True)
         self.Button_Mode.setObjectName("Button_Mode")
         self.Accu_Bar = QtWidgets.QProgressBar(Form)
         self.Accu_Bar.setGeometry(QtCore.QRect(20, 10, 361, 61))
-        self.Accu_Bar.setProperty("value", 24)
+        self.Accu_Bar.setProperty("value", 100)
         self.Accu_Bar.setObjectName("Accu_Bar")
         self.lcdNumber = QtWidgets.QLCDNumber(Form)
         self.lcdNumber.setGeometry(QtCore.QRect(20, 80, 361, 151))
         self.lcdNumber.setObjectName("lcdNumber")
 
         self.retranslateUi(Form)
-        self.Button_Mode.clicked.connect(Form.Bttn_mode)
+        self.Button_Mode.clicked.connect(self.Bttn_mode)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.Button_Mode.setText(_translate("Form", "Handmatig"))
+        self.Button_Mode.setText(_translate("Form", "Handmatige Besturing"))
 
 
 if __name__ == "__main__":
